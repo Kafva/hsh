01cbea28206c2d0657b0bb168d78ff24d5b88c3d
Kafva <36083692+Kafva@users.noreply.github.com>
2023-12-17 11:47:28 +0100

    Indentation

--- src/Md5.hs
+++ src/Md5.hs
@@ -7,27 +7,27 @@ import Data.Bits ((.&.), (.|.), complement, xor)  -- '&', '|' etc.
 import Types
 
 {-
-  A 16 byte buffer divided into 4 (32 bit) registers is used to compute
-  the digest (a b c d).
-  Word ~ Unsigned
+    A 16 byte buffer divided into 4 (32 bit) registers is used to compute
+    the digest (a b c d).
+    Word ~ Unsigned
 -}
 data Digest = Digest {
-  a :: Word32,
-  b :: Word32,
-  c :: Word32,
-  d :: Word32
+    a :: Word32,
+    b :: Word32,
+    c :: Word32,
+    d :: Word32
 }
 
 {-
-  (1) PADDING BITS
-  Append a '1' bit and fill with '0' until the bit-length of the
-  input adheres to:
+    (1) PADDING BITS
+    Append a '1' bit and fill with '0' until the bit-length of the
+    input adheres to:
         input % 512 == 448 ~
         input % 64  == 56
 
-  Input will always be a multiple of 8.
-  Padding should be performed even if the input length already has 448
-  as the remainder mod 512.
+    Input will always be a multiple of 8.
+    Padding should be performed even if the input length already has 448
+    as the remainder mod 512.
 -}
 padBlock :: [Word8] -> [Word8]
 padBlock bytes = if (mod (length bytes) (div 512 8) /= (div 448 8))
@@ -35,7 +35,7 @@ padBlock bytes = if (mod (length bytes) (div 512 8) /= (div 448 8))
                  else bytes
 
 
-{- 
+{-
   (2) APPEND LENGTH
   Append the 64 bit representation of the original length of the message
 -}
@@ -43,9 +43,10 @@ appendLength :: [Word8] -> Int64 -> [Word8]
 appendLength bytes len = bytes ++ (ByteStringLazy.unpack $ Binary.encode len)
 
 
--- Auxillary functions --
--- Each of the auxillary functions are defined to act over bits
--- in each word and map 3 words onto 1.
+{-
+    Each of the auxillary functions are defined to act over bits
+    in each word and map 3 words onto 1.
+-}
 f :: Word8 -> Word8 -> Word8 -> Word8
 f x y z = (x .&. y) .|. ((complement x) .&. z)
 
@@ -59,10 +60,10 @@ i :: Word8 -> Word8 -> Word8 -> Word8
 i x y z = xor y $ x .&. (complement z)
 
 {-
-  https://www.rfc-editor.org/rfc/pdfrfc/rfc1321.txt.pdf
+    https://www.rfc-editor.org/rfc/pdfrfc/rfc1321.txt.pdf
 
-  Hash algorithms map a variable length bit-string onto a fixed length
-  bit-string, 128 bits (16 bytes) in the case of MD5.
+    Hash algorithms map a variable length bit-string onto a fixed length
+    bit-string, 128 bits (16 bytes) in the case of MD5.
 -}
 hash :: [Char] -> [Word8]
 hash inputData = do
