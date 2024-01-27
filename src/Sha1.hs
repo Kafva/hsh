7f5761d1863cd37a222b7750d4d27ffbb7e7cb85
Kafva <36083692+Kafva@users.noreply.github.com>
2024-01-27 23:28:22 +0100

    wip: sha1

--- src/Sha1.hs
+++ src/Sha1.hs
@@ -2,6 +2,7 @@ module Sha1 (hash) where
 
 import Control.Monad.Reader
 import Data.Binary (Word8, Word32)
+import Data.Bits ((.&.), (.|.), complement, xor, rotateL, rotateR)
 import Log (trace')
 import Types (Config)
 import Util (padInput,
@@ -10,6 +11,26 @@ import Util (padInput,
              word8toWord32Array,
              word32ArrayToBlocks)
 
+f :: Int -> Word32 -> Word32 -> Word32 -> Word32
+f t b c d
+    -- f(t;B,C,D) = (B AND C) OR ((NOT B) AND D)
+    | 0 <= t && t <= 19 = (b .&. c) .|. ( (complement b) .&. d)
+    -- f(t;B,C,D) = (B AND C) OR (B AND D) OR (C AND D)
+    | 40 <= t && t <= 59 = (b .&. c) .|. (b .&. d) .|. (c .&. d)
+    -- f(t;B,C,D) = B XOR C XOR D
+    | otherwise = xor b (xor c d)
+
+
+getK :: Int -> Word32
+getK i
+    | i <= 19   = 0x5a827999
+    | i <= 39   = 0x6ed9eba1
+    | i <= 59   = 0x8f1bbcdc
+    | otherwise = 0xca62c1d6
+
+-- S^n(X)  = (X << n) OR (X >> 32-n)
+auxS :: Int -> Word32 -> Word32
+auxS n x = (rotateL x n) .|. (rotateR x (32 - n))
 
 {-
  - https://www.ietf.org/rfc/rfc3174.txt
@@ -25,6 +46,8 @@ hash bytes = do
                                    0x98badcfe,
                                    0x10325476,
                                    0xc3d2e1f0]
+
+    -- W(t) = S^1(W(t-3) XOR W(t-8) XOR W(t-14) XOR W(t-16)) <<< 1
 
 
     return $ concatMap word32ToWord8Array startDigest
