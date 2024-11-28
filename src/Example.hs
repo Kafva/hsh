3f365c89d5d072c427e1d68f206cbe3e3c647174
Kafva <kafva.one@gmail.com>
2024-11-28 07:29:21 +0100

    wip testing parallel

--- src/Example.hs
+++ src/Example.hs
@@ -1,46 +1,32 @@
-import Control.Parallel (par, pseq)
-import Control.Concurrent (threadDelay)
-import System.CPUTime (getCPUTime)
-import Text.Printf (printf)
-import Control.Monad (forM)
+import Control.Parallel (pseq, par)
+--import Control.Concurrent (threadDelay)
 
 timeConsumingTask :: Int -> IO Int
-timeConsumingTask n = do
-    threadDelay (n * 1000 * 1000)  -- Sleep for n seconds
-    putStrLn $ "Task done [" ++ show n ++ "]"
+timeConsumingTask _ = do
+    --threadDelay (n * 1000 * 1000)  -- Sleep for n seconds
+    let x = sum [0..500_000_000]
+    putStrLn $ "Task done [" ++ (show x) ++ "]"
     return 7
-
-runSeq :: IO ()
-runSeq = do
-     start <- getCPUTime
-     mapM_ timeConsumingTask [1, 1, 1]
-     end <- getCPUTime
-     let diff = fromIntegral (end - start) / (10^12)
-     printf "Sequential execution time: %0.3f sec\n" (diff :: Double)
 
 runPar :: IO ()
 runPar = do
-    start <- getCPUTime
+    putStrLn "Running in parallel..."
+    let a = timeConsumingTask 1
+        b = timeConsumingTask 1
+        c = timeConsumingTask 1
+    r <- a `par` b `par` c `pseq` sequence [a,b,c]
+    print $ show r
 
-
-    printf "Starting...\n"
-    results <- forM [1..4] $ \i -> do
-        let result = timeConsumingTask 5
-        result `par` return result
-    
-    -- Wait for parallel execution to finish, `seq` makes sure that both of its 
-    -- arguments are evaluated before returning.
-    printf "Waiting...\n"
-    mapM_ (\r -> r `seq` return ()) results
-    
-    -- Unwrap from [Reader Config [Word8]] -> [[Word8]]
-    ts <- sequence results
-
-    end <- getCPUTime
-    let diff = fromIntegral (end - start) / (10^12)
-    printf "Parallel execution time: %0.3f sec\n" (diff :: Double)
+runSeq :: IO ()
+runSeq = do
+    putStrLn "Running \"normally\"..."
+    a <- timeConsumingTask 1
+    b <- timeConsumingTask 1
+    c <- timeConsumingTask 1
+    let r = [a,b,c]
+    print $ show r
 
 main :: IO ()
 main = do
-    runSeq
+    --runSeq
     runPar
